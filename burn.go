a80e912b228a1e520fccaa36b7e155eaadb61a73
sugyan <sugi1982@gmail.com>
2017-08-09 00:42:24 +0900

    animation

--- burn.go
+++ burn.go
@@ -7,7 +7,9 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-var colors = []uint16{16, 52, 88, 124, 160, 196, 202, 208, 214, 220, 226, 231}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func Run() {
 	if err := termbox.Init(); err != nil {
@@ -17,69 +19,31 @@ func Run() {
 
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	termbox.SetOutputMode(termbox.Output256)
-	colSize, rowSize := termbox.Size()
-	rowSize += 2
-	cells := make([][]float32, rowSize)
-	for i := range cells {
-		cells[i] = make([]float32, colSize)
-	}
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	cells[rowSize-1][0] = rand.Float32()
-	for i := 1; i < colSize; i++ {
-		val := cells[rowSize-1][i-1] + r.Float32()*0.6 - 0.3
-		if val >= 1.0 {
-			val = 1.0
-		}
-		if val <= 0.0 {
-			val = 0.0
-		}
-		cells[rowSize-1][i] = val
-	}
-	for i := rowSize - 2; i >= 0; i-- {
-		for j := 0; j < colSize; j++ {
-			num, sum := 1, cells[i+1][j]
-			if j-1 > 0 {
-				num++
-				sum += cells[i+1][j-1]
-			}
-			if j+1 < colSize-1 {
-				num++
-				sum += cells[i+1][j+1]
-			}
-			if i+2 < rowSize-1 {
-				num++
-				sum += cells[i+1][j]
-			}
-			val := sum / float32(num)
-			val += r.Float32()*0.04 - 0.02
-			if val >= 1.0 {
-				val = 1.0
-			}
-			if val <= 0.0 {
-				val = 0.0
-			}
-			cells[i][j] = val
-		}
-	}
+	t := newTerminal()
 
-	for i := 0; i < rowSize; i++ {
-		for j := 0; j < colSize; j++ {
-			value := cells[i][j]
-			color := colors[int(value*float32(len(colors)-1))]
-			termbox.SetCell(j, i, ' ', termbox.ColorDefault, termbox.Attribute(color+1))
+	ch := make(chan termbox.Event)
+	go func() {
+		for {
+			ch <- termbox.PollEvent()
 		}
-	}
-	if err := termbox.Flush(); err != nil {
-		panic(err)
-	}
+	}()
 
 loop:
 	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			if ev.Key == termbox.KeyEsc {
-				break loop
+		select {
+		case ev := <-ch:
+			switch ev.Type {
+			case termbox.EventKey:
+				if ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyEsc {
+					break loop
+				}
 			}
+			break
+		default:
+			if err := t.update(); err != nil {
+				panic(err)
+			}
+			time.Sleep(time.Millisecond * 150)
 		}
 	}
 }
---
+++ term.go
@@ -0,0 +1,85 @@
+package termburn
+
+import (
+	"math"
+	"math/rand"
+
+	"github.com/nsf/termbox-go"
+)
+
+var colors = []uint16{16, 52, 88, 124, 160, 196, 202, 208, 214, 220, 226, 231}
+
+type terminal struct {
+	col, row int
+	values   [][]float32
+}
+
+func newTerminal() *terminal {
+	col, row := termbox.Size()
+	row++
+	values := make([][]float32, row)
+	for i := 0; i < row; i++ {
+		values[i] = make([]float32, col)
+	}
+	for i := 0; i < col; i++ {
+		values[row-1][i] = rand.Float32()
+	}
+	return &terminal{
+		col:    col,
+		row:    row,
+		values: values,
+	}
+}
+
+func (t *terminal) update() (err error) {
+	for i := 0; i < t.col; i++ {
+		val := t.values[t.row-1][i] + rand.Float32()*0.5 - 0.25
+		t.values[t.row-1][i] = clip(val)
+	}
+	for i := t.row - 2; i >= 0; i-- {
+		for j := 0; j < t.col; j++ {
+			num, sum := 1, t.values[i+1][j]
+			if j-1 > 0 {
+				num++
+				sum += t.values[i+1][j-1]
+			}
+			if j+1 < t.col-1 {
+				num++
+				sum += t.values[i+1][j+1]
+			}
+			if i+2 < t.row-1 {
+				num++
+				sum += t.values[i+1][j]
+			}
+			val := sum / float32(num)
+			a := float32(math.Pow(math.E, math.Log(0.001)/float64(t.row)))
+			val += 0.1 * (rand.Float32() + a/2.0 - 1.0)
+			t.values[i][j] = clip(val)
+		}
+	}
+	return t.render()
+}
+
+func (t *terminal) render() (err error) {
+	for i := 0; i < t.row-1; i++ {
+		for j := 0; j < t.col; j++ {
+			value := t.values[i][j]
+			color := colors[int(value*float32(len(colors)-1))]
+			termbox.SetCell(j, i, ' ', termbox.ColorDefault, termbox.Attribute(color+1))
+		}
+	}
+	if err = termbox.Flush(); err != nil {
+		return
+	}
+	return nil
+}
+
+func clip(value float32) float32 {
+	if value > 1.0 {
+		return 1.0
+	}
+	if value < 0.0 {
+		return 0.0
+	}
+	return value
+}
